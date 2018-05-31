d15f74e4774430776cba086666e0505894aa6e9c
Anita Nandi <anita.k.nandi@gmail.com>
2018-05-31 16:25:59 +0100

    Add button to pick video

--- app/src/main/java/uk/co/jacoblever/stillcapturer/MainActivity.java
+++ app/src/main/java/uk/co/jacoblever/stillcapturer/MainActivity.java
@@ -1,11 +1,16 @@
 package uk.co.jacoblever.stillcapturer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +44,25 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Video"),101);
+            }
+        });
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 101) {
+                Uri selectedImageUri = data.getData();
+                mTextMessage.setText(selectedImageUri.getPath());
+            }
+        }
+    }
 }
